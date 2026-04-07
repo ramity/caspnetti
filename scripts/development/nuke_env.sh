bed99136bf8bc39e8c6e61ffbcc969e1eecd5e64
ramity <lewisclbrown@gmail.com>
2026-04-07 13:25:46 -0500

    added env copy and env nuke scripts to workflow + updated env scripts to use mariadb over mssql + created nuke env scripts for development and prod DANGEROUS

---
+++ scripts/development/nuke_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove .env.local files for all services:
+rm docker/development/adminer/.env.local
+rm docker/development/aspnet/.env.local
+rm docker/development/mariadb/.env.local
+rm docker/development/nginx/caspnetti-ssl.conf.local
+rm docker/development/nginx/caspnetti.conf.local
+rm docker/development/vue/.env.local
